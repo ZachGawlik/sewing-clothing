a46d4d32f4f03074b0a330c8b71ff35739e5afd1
Zach Gawlik <ZachGawlik@users.noreply.github.com>
2021-12-22 23:15:11 -0500

    build: add page suffix to allow colocating tests

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  pageExtensions: [
+    'page.tsx',
+    'page.ts',
+    'page.jsx',
+    'page.js',
+    'page.mdx',
+    'page.md',
+  ],
 };
