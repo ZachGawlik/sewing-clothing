89da2476498fabffebf610e6ea0be26948464d4a
Zach Gawlik <ZachGawlik@users.noreply.github.com>
2021-12-24 19:33:11 -0500

    style: Bump eslint, prettier

--- pages/tools/index.page.tsx
+++ pages/tools/index.page.tsx
@@ -1,7 +1,6 @@
 import type { NextPage } from 'next';
 import Link from 'next/link';
 import Head from 'next/head';
-import styles from '../../styles/Home.module.css';
 
 const ToolsPage: NextPage = () => {
   return (
