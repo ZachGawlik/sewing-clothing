26d85d389d4e6f37be2c44a22512a19ec09943b5
Zach Gawlik <ZachGawlik@users.noreply.github.com>
2022-02-24 18:10:40 -0500

    feat(404): add 404

---
+++ pages/404.page.tsx
@@ -0,0 +1,22 @@
+import Head from 'next/head';
+import styles from '../styles/Home.module.css';
+import PageHeader from './components/PageHeader';
+
+const FourOhFourPage = () => {
+  return (
+    <div className={`${styles.container}`}>
+      <Head>
+        <title>404 | sewing.clothing</title>
+      </Head>
+      <PageHeader className="max-w-prose lg:max-w-none" />
+      <main className={`${styles.main} prose prose-invert`}>
+        <h1>404</h1>
+        <p>
+          Page not found. Go back to <a href="/">home</a>.
+        </p>
+      </main>
+    </div>
+  );
+};
+
+export default FourOhFourPage;
